094b3baefd7265ac6d19665c456b4044c1e8c62a
Kent Andersen <kentareandersen@gmail.com>
2013-11-04 23:16:27 +0100

    fixed scraper

--- src/scraper.js
+++ src/scraper.js
@@ -1,26 +1,54 @@
+var multipleExec = function(reg, matcher, callback, context) {
 
-var reg = /background.*:.*url\((.*)\)/g;
-
-exports.findAllBackgrounds = function (cssData) {
-
-    var searchData = cssData;
+    var searchData = matcher;
     var search = reg.exec(searchData);
-    var backgrounds = [];
-
+    var offset = 0;
 
     while (search) {
         var fullMatch = search[0];
-        var lastSearch = search.index + fullMatch.length;
 
-        backgrounds.push({
-            startIndex: search.index,
-            endIndex: lastSearch,
-            background: fullMatch
-        });
+        var start = search.index + offset;
+        var endIndex = start + fullMatch.length;
 
-        searchData.slice(lastSearch)
+        callback.call(context || this, start, endIndex, search);
+
+        offset = endIndex;
+
+        searchData = matcher.substr(offset);
+
         search = reg.exec(searchData);
     }
 
+
+};
+
+
+exports.findAllBackgrounds = function (cssData) {
+
+    var backgrounds = [];
+    var reg = /background.*:.*url\((.*)\).*?(;|})/m;
+    
+    multipleExec(reg, cssData, function (start, endIndex, extracted) {
+        backgrounds.push({
+            startIndex: start,
+            endIndex:   endIndex,
+            background: extracted[0]
+        });
+    });
+
     return backgrounds;
+};
+
+exports.getImageUrl = function (background) {
+    var reg = /url\((.*?)\)/;
+    var imageUrls = [];
+
+    multipleExec(reg, background, function (start, endIndex, extracted) {
+        var imagePath = extracted[1];
+        imagePath = imagePath.replace(/\"|\'/g, "");
+
+        imageUrls.push(imagePath);
+    });
+
+    return imageUrls;
 }
