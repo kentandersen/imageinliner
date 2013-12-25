9ace5ac78f879fc63ed2674963c11f24a5f90acf
Kent Andersen <kentareandersen@gmail.com>
2013-12-25 16:19:16 +0100

    added tests for root based, compressed and compressed root based css files

--- test/cli.js
+++ test/cli.js
@@ -1,15 +1,14 @@
 var fs = require("fs");
 var path = require("path");
 var assert = require("assert")
+var _ = require("underscore");
 require('shelljs/global');
 
 
 describe('cli interface', function() {
-    var inputFile = "test/fixtures/fixture.css";
+    var filesystemRoot = path.dirname(fs.realpathSync(__filename));
 
     var outputFolder = "output/";
-    var outputFile = path.join(outputFolder, "file.css");
-
     beforeEach(function () {
         mkdir(outputFolder);
     });
@@ -18,52 +17,109 @@ describe('cli interface', function() {
         rm("-fr", outputFolder);
     });
 
-    it("should run correctly", function() {
-        var res = exec("./bin/imageinliner -i "+ inputFile +" -o " + outputFile);
-        assert.equal(res.code, 0);
+    var buildArguments = function(options) {
+        return _.map(arguments, function(argument) {
+            var args = _.map(argument, function(value, key) {
+                var arg = (key.length > 1) ? "--" : "-";
+
+                if(value === true) {
+                    return arg + key;
+                } else if(value === false) {
+                    // do nothing
+                } else {
+                    return arg + key +  " " + value;
+                }
+            });
+            return " " + args.join(" ");
+        }).join(" ");
+    };
+
+    var assertCli = function(inputFile, options) {
+
+        var outputFile = path.join(outputFolder, "file.css");
+
+        it("should run correctly", function() {
+            var args = buildArguments({i: inputFile, o: outputFile}, options);
+            var res = exec("./bin/imageinliner" + args);
+            assert.equal(res.code, 0);
+        });
+
+        it("should create output file", function() {
+            var args = buildArguments({i: inputFile, o: outputFile}, options);
+            exec("./bin/imageinliner" + args);
+
+            assert.ok(fs.existsSync(outputFile));
+        });
+
+        it("should create output file with filesystem root paths", function() {
+            var rootInputFile = path.join(filesystemRoot, "..", inputFile);
+            var rootOutputFile = path.join(filesystemRoot, "..", outputFolder, "file.css");
+
+            var args = buildArguments({i: rootInputFile, o: rootOutputFile}, options);
+            exec("./bin/imageinliner" + args);
+
+            assert.ok(fs.existsSync(outputFile));
+        });
+
+        it("should create output file when --in and --out param is passed", function() {
+            var args = buildArguments({in: inputFile, out: outputFile}, options);
+            exec("./bin/imageinliner" + args);
+
+            assert.ok(fs.existsSync(outputFile));
+        });
+
+        it("should create output file in multiple sub-folders", function() {
+            var deepOutputFile = path.join(outputFolder, "some/folder/again/file.css");
+
+            var args = buildArguments({i: inputFile, o: deepOutputFile}, options);
+            exec("./bin/imageinliner" + args);
+
+            assert.ok(fs.existsSync(deepOutputFile));
+        });
+
+        it("should not include linebreaks when --compress param is passed", function() {
+            var args = buildArguments({i: inputFile, o: outputFile, compress: true}, options);
+            exec("./bin/imageinliner" + args);
+
+            var cssData = fs.readFileSync(outputFile, "utf-8");
+            assert.equal(cssData.match(/\n/g), null);
+        });
+
+        it("should overwrite itself when --overwrite param is passed", function() {
+            var copiedFolder = "test/fixtures";
+            cp("-r", copiedFolder, outputFolder);
+
+            var fileName = path.basename(inputFile);
+
+            var copiedFile = path.join(outputFolder, "fixtures", fileName);
+
+            var args = buildArguments({i: inputFile, out: outputFile}, options);
+            exec("./bin/imageinliner"+ args);
+
+            args = buildArguments({i: copiedFile, overwrite: true}, options);
+            exec("./bin/imageinliner"+ args);
+
+            var originalData = fs.readFileSync(outputFile, "utf-8");
+            var overwriteData = fs.readFileSync(copiedFile, "utf-8");
+
+            assert.equal(originalData, overwriteData);
+        });
+    };
+
+    assertCli("test/fixtures/style.css");
+    assertCli("test/fixtures/compressed.css");
+    assertCli("test/fixtures/root.css", {
+        rootPath: "test"
     });
-
-    it("should create output file", function() {
-        exec("./bin/imageinliner -i "+ inputFile +" -o " + outputFile);
-
-        assert.ok(fs.existsSync(outputFile));
+    assertCli("test/fixtures/root.css", {
+        rootPath: filesystemRoot
     });
-
-    it("should create output file when --in and --out param is passed", function() {
-        exec("./bin/imageinliner --in "+ inputFile +" --out " + outputFile);
-
-        assert.ok(fs.existsSync(outputFile));
+    assertCli("test/fixtures/compressedRoot.css", {
+        rootPath: "test"
     });
-
-    it("should create output file in multiple sub-folders", function() {
-        var deepOutputFile = path.join(outputFolder, "some/folder/again/file.css");
-        exec("./bin/imageinliner -i "+ inputFile +" -o " + deepOutputFile);
-
-        assert.ok(fs.existsSync(deepOutputFile));
+    assertCli("test/fixtures/compressedRoot.css", {
+        rootPath: filesystemRoot
     });
-
-    it("should not include linebreaks when --compress param is passed", function() {
-        exec("./bin/imageinliner -i "+ inputFile +" -o " + outputFile + " --compress");
-
-        var cssData = fs.readFileSync(outputFile, "utf-8");
-        assert.equal(cssData.match(/\n/g), null);
-    });
-
-    it("should overwrite itself when --overwrite param is passed", function() {
-        var copiedFolder = "test/fixtures";
-        cp("-r", copiedFolder, outputFolder);
-
-        var copiedFile = path.join(outputFolder, "fixtures", "fixture.css");
-
-        exec("./bin/imageinliner -i "+ inputFile +" -o " + outputFile);
-        exec("./bin/imageinliner -i "+ copiedFile +" --overwrite");
-
-        var originalData = fs.readFileSync(outputFile, "utf-8");
-        var overwriteData = fs.readFileSync(copiedFile, "utf-8");
-
-        assert.equal(originalData, overwriteData);
-    });
-
 });
 
 
