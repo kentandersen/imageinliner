f92432e3650c6e4a6c634925be1bd6b77cba598b
Kent Andersen <kentareandersen@gmail.com>
2014-02-01 15:06:58 +0100

    fixed test after method change

--- test/inliner.js
+++ test/inliner.js
@@ -1,10 +1,28 @@
 var assert = require("assert")
+var fs = require("fs");
 var inliner = require("../src/inliner.js");
 var _ = require("underscore");
 
 describe('inliner with fixture', function(){
 
-    var assertInliner = function(cssFile, options) {
+    var assertInlinerString = function(cssFile, options) {
+        var css = fs.readFileSync(cssFile, "utf-8");
+
+        var buildArguments = function(options) {
+            return _.defaults(options || {}, {
+                imageBasePath:      "test/fixtures",
+                maxImageFileSize:   10240,
+                compressOutput:     true
+            });
+        };
+
+        it("should return string when using files", function() {
+            result = inliner.css(css, buildArguments(options));
+            assert.equal(typeof result, "string");
+        });
+    };
+
+    var assertInlinerFile = function(cssFile, options) {
 
         var buildArguments = function(options) {
             return _.defaults(options || {}, {
@@ -13,18 +31,27 @@ describe('inliner with fixture', function(){
             });
         };
 
-        it("should return string", function() {
-            result = inliner(cssFile, buildArguments(options));
+        it("should return string when using files", function() {
+            result = inliner.file(cssFile, buildArguments(options));
             assert.equal(typeof result, "string");
         });
     };
 
-    assertInliner("test/fixtures/style.css");
-    assertInliner("test/fixtures/compressed.css");
-    assertInliner("test/fixtures/root.css", {
+    assertInlinerFile("test/fixtures/style.css");
+    assertInlinerFile("test/fixtures/compressed.css");
+    assertInlinerFile("test/fixtures/root.css", {
         rootImagePath: "test"
     });
-    assertInliner("test/fixtures/compressedRoot.css", {
+    assertInlinerFile("test/fixtures/compressedRoot.css", {
+        rootImagePath: "test"
+    });
+
+    assertInlinerString("test/fixtures/style.css");
+    assertInlinerString("test/fixtures/compressed.css");
+    assertInlinerString("test/fixtures/root.css", {
+        rootImagePath: "test"
+    });
+    assertInlinerString("test/fixtures/compressedRoot.css", {
         rootImagePath: "test"
     });
 });
